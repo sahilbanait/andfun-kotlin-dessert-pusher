bbb821c945093d5bf24bc45d3631bed2b00a6129
sahilbanait <77091616+sahilbanait@users.noreply.github.com>
2021-07-16 09:01:20 +0800

    Initial commit

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -75,6 +76,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
+        dessertTimer = DessertTimer(this.lifecycle)
+
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -153,6 +156,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         Log.i("MainAcitivity", "onStart:called ")
+        dessertTimer.startTimer()
         super.onStart()
     }
 
@@ -173,6 +177,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         Timber.i("OnStop")
+        dessertTimer.stopTimer()
         super.onStop()
     }
 
